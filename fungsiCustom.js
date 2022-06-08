2dfcd649c3c7e201b930906d5e9b8a60accfee38
mandan koding <mandankoding@gmail.com>
2022-06-08 14:02:28 +0700

    upload

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require('fs')
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,38 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+
+  fs.readFile(file1, 'utf-8', (err, data) => {
+
+    const dataJson1 = JSON.parse(data);
+    if (!dataJson1.message.split(" ")[1]) return fnCallback("Data tidak sesuai", null);
+
+    fs.readFile(file2, 'utf-8', (err, data) => {
+
+      const dataJson2 = JSON.parse(data);
+      if (!dataJson2[0].message.split(" ")[1]) return fnCallback("Data tidak sesuai", null);
+
+      fs.readFile(file3, 'utf-8', (err, data) => {
+
+        const dataJson3 = JSON.parse(data);
+        if (!dataJson3[0].data.message.split(" ")[1]) return fnCallback("Data tidak sesuai", null);
+
+        const result = [
+          dataJson1.message.split(" ")[1],
+          dataJson2[0].message.split(" ")[1],
+          dataJson3[0].data.message.split(" ")[1],
+        ]
+
+        return fnCallback(null, result);
+
+      }); // file 3
+
+    }); // file 2
+
+  }); // file 1
+
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
